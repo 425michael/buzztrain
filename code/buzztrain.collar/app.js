a70ecc2ecf0d64b56e11116d20cddad8becb56e9
Ethan Ott <ethan.ott@gmail.com>
2015-03-07 18:40:39 -0800

    complete .device app.js code

--- code/buzztrain.collar/app.js
+++ code/buzztrain.collar/app.js
@@ -1,25 +1,110 @@
+// Software Configuration========================
+var mraa = require('mraa');
 //var cylon = require('cylon');
 var socket = require('socket.io-client')
-    //.connect('http://buzztrain.azurewebsites.net');
-    .connect('http://192.168.1.2:3000');
+	.connect('http://buzztrain.azurewebsites.net');
 
-//work on a real device
-//cylon.robot({
-//    name: 'edison',
-//    connections: { edison: { adaptor: 'intel-iot' } },
-//    devices: { monkey: { driver: 'direct-pin', pin: 2 } },
-//    work: function (edison) {
-        socket.on('command', function (cmd) {
-			console.log(cmd);
-		//            edison.monkey.digitalWrite(1);
-//            setTimeout(function () { edison.monkey.digitalWrite(0); }, 2000);
-//        })
-//    }
-//}).start();
+// HW Configuration =============================
+var pin13 = new mraa.Gpio(13);
+pin13.dir(mraa.DIR_OUT);
+pin13.write(0);
 
-////test in a console window
-//console.log("Requesting to be target...");
-//socket.emit('setTarget');
-//socket.on('command', function (cmd) {
-//    console.log('Received a command: ' + cmd);
-//})
+var buzzer = new mraa.Gpio(3);
+buzzer.dir(mraa.DIR_OUT);
+
+// Main code ===================================
+socket.on('command',function (cmd) {
+	switch (cmd) {
+		case 'sit':
+			cmd1();
+			break;
+		case 'laydown':
+			cmd2();
+			break;
+		case 'stay':
+			cmd3();
+			break;
+	}
+});
+
+//cmd1(); // one second beep
+//cmd2(); // two half second beeps
+//cmd3(); // three very short beeps
+
+
+
+// Command 1 ===================================
+function cmd1() {
+	console.log ( 'Command 1 Triggered' );
+	buzzer.write(1);
+	pin13.write(1);
+	setTimeout( cmd1_off2,1000);
+}
+function cmd1_off1() {
+	buzzer.write(0);
+	pin13.write(0);
+	setTimeout( cmd1_on2,500);
+}
+function cmd1_on2() {
+	buzzer.write(1);
+	pin13.write(1);
+	setTimeout( cmd1_off2,500);
+}
+function cmd1_off2() {
+	buzzer.write(0);
+	pin13.write(0);
+}
+
+// Command 2 ===================================
+function cmd2() {
+	console.log ( 'Command 2 Triggered' );
+	buzzer.write(1);
+	pin13.write(1);
+	setTimeout( cmd2_off1,500);
+}
+function cmd2_off1() {
+	buzzer.write(0);
+	pin13.write(0);
+	setTimeout( cmd2_on2,250);
+}
+function cmd2_on2() {
+	buzzer.write(1);
+	pin13.write(1);
+	setTimeout( cmd2_off2,500);
+}
+function cmd2_off2() {
+	buzzer.write(0);
+	pin13.write(0);
+}
+
+// Command 3 ===================================
+function cmd3() {
+	console.log ( 'Command 3 Triggered' );
+	buzzer.write(1);
+	pin13.write(1);
+	setTimeout( cmd3_off1,250);
+}
+function cmd3_off1() {
+	buzzer.write(0);
+	pin13.write(0);
+	setTimeout( cmd3_on2,100);
+}
+function cmd3_on2() {
+	buzzer.write(1);
+	pin13.write(1);
+	setTimeout( cmd3_off2,250);
+}
+function cmd3_off2() {
+	buzzer.write(0);
+	pin13.write(0);
+	setTimeout( cmd3_on3,100);
+}
+function cmd3_on3() {
+	buzzer.write(1);
+	pin13.write(1);
+	setTimeout( cmd3_off3,250);
+}
+function cmd3_off3() {
+	buzzer.write(0);
+	pin13.write(0);
+}
